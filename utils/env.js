23a1a91bfd2044c97c7fa476134fcb79dbd927c6
ndeviant <kpoccep@yandex.ru>
2020-09-15 01:09:16 +0300

    updated deps

--- utils/env.js
+++ utils/env.js
@@ -1,5 +1,8 @@
 // tiny wrapper with default env vars
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "development";
+if (!process.env.PORT) process.env.PORT = 3000;
+
 module.exports = {
-  NODE_ENV: (process.env.NODE_ENV || "development"),
-  PORT: (process.env.PORT || 3000)
+	NODE_ENV: process.env.NODE_ENV,
+	PORT: process.env.PORT,
 };
